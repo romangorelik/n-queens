afd0eb3ca322d05ac94cc49903989939767f8e07
Roman Gorelik <romang31@gmail.com>
2018-06-20 19:47:32 -0400

    Added first solver

--- src/solvers.js
+++ src/solvers.js
@@ -16,18 +16,38 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board ({n: n});
+  var rooksLeft = n;
+  
+  var addPiece = function (row, col) {
+    solution.togglePiece(row, col);
+    rooksLeft--;
+    if (solution.hasAnyRooksConflicts()) {
+      solution.togglePiece(row,col);
+      rooksLeft++;
+    }
+  };
+
+  var backTrack = function (row, col) {
+    for (var i = row; i < solution.rows().length; i++) {
+      var column = solution.rows()[i];
+      for (var j = col; j < column.length; j++) {
+        if (rooksLeft > 0) {
+          addPiece(i, j);
+        }
+      }
+    }
+  }
+
+  backTrack(0, 0);
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
